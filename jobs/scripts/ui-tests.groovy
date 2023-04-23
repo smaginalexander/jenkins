edc37f985d09281b888a4294623456e8cdaa040c
alexander <smaginalexander@yandex.ru>
2023-04-24 00:42:45 +0300

    fix

---
+++ jobs/scripts/ui-tests.groovy
@@ -0,0 +1,21 @@
+timeout(60) {
+    node('maven') {
+        stage('Checkout') {
+            checkout scm
+        }
+        stage('Run UI tests') {
+            node('maven') {
+                catchError(buldResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+                    build(job: 'ui-tests',
+                            parameters: [
+                                    string(name: 'BRANCH', value: BRANCH),
+                                    string(name: 'BASE_URL', value: BASE_URL),
+                                    string(name: 'BROWSER', value: BROWSER),
+                                    string(name: 'BROWSER_VERSION', value: BROWSER_VERSION),
+                                    string(name: 'GRID_URL', value: GRID_URL)
+                            ])
+                }
+            }
+        }
+    }
+}
